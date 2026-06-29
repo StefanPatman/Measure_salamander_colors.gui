1da91630478bc628dbec4a0b3540fe8f6f3baddb
StefanPatman <stefanpatman91@gmail.com>
2026-06-30 02:41:38 +0300

    Fix pyinstaller hook

--- src/itaxotools/__pyinstaller/hook-itaxotools.salamander_colors.py
+++ src/itaxotools/__pyinstaller/hook-itaxotools.salamander_colors.py
@@ -1,3 +1,5 @@
-from PyInstaller.utils.hooks import collect_data_files
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
 
 datas = collect_data_files("itaxotools.salamander_colors")
+
+hiddenimports = collect_submodules("itaxotools.salamander_colors.tasks", filter=lambda name: True)
